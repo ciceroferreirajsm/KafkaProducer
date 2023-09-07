96934d6154ec6385f36d6b1353b5342679697a90
cferreira-at-692031961205 <ciceronascimentu@gmail.com>
2023-09-07 19:54:01 -0300

    Adicionar arquivos de projeto.

---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var config = new ProducerConfig { 
+                BootstrapServers = "sulky.srvs.cloudkafka.com:9094",
+                SaslUsername = "pahgpgwu",                 
+                SaslPassword = "kBLow9zHco2bD7uO7UQYn0dVe14L7cdl",
+                SaslMechanism = SaslMechanism.ScramSha512,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                EnableSslCertificateVerification = false
+            
+            };
+
+            using var p = new ProducerBuilder<Null, string>(config).Build();
+
+            {
+                try
+                {
+                    var count = 0;
+                    while (true)
+                    {
+                        var dr = await p.ProduceAsync("pahgpgwu-default",
+                            new Message<Null, string> { Value = $"test: {count++}" });
+
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
+
+                        Thread.Sleep(2000);
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
